330a4a69e0691bd96d6dfdff8ebb95b6625f5e77
luanbianchini <luanbianchine1@gmail.com>
2026-01-20 12:17:26 -0300

    hotfix: typo

--- src/utils/featuresMapper.ts
+++ src/utils/featuresMapper.ts
@@ -2,6 +2,6 @@ import { Features } from "../features/common/featuresEnum";
 
 export const featureMapper = {
   [Features.FINANCIAL_DASHBOARD]: "Dashboard Financeiro",
-  [Features.SALES_REPORTS]: "Relatórios de Vendas",
+  [Features.SALES_REPORTS]: "Relatório de Vendas",
   [Features.UPLOAD_PRODUCTS_EXCEL]: "Upload de Produtos via Excel",
 };
